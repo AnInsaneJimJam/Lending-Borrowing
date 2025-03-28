085bf68b8778e0a644d03ba2b8c937b9c5277610
12anand34 <anand47bansal#gmail.com>
2025-03-28 09:06:38 +0000

    Debt interest handled

--- src/BLP.sol
+++ src/BLP.sol
@@ -14,7 +14,8 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
  * @notice Liquidate function
  */
 contract BLP is ReentrancyGuard {
-    uint256 private constant INTEREST_RATE = 8;
+    uint256 private constant BORROWING_INTEREST_RATE = 8;
+    uint256 private constant LENDING_INTEREST_RATE = 3;
     uint256 private constant SECONDS_IN_YEAR = 31536000;
     uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
     uint256 private constant PRECISION = 1e18;
@@ -30,14 +31,14 @@ contract BLP is ReentrancyGuard {
     }
 
     struct Debt {
-        uint256 usdtBorrowed;
+        uint256 amount;
         uint256 timestamp;
     }
 
     mapping(address => Deposit[]) public userDeposits;
-    address wethPriceFeed; 
-    address usdtPriceFeed; 
-    mapping(address user =>  uint256 amount) public collateralDeposited;
+    address wethPriceFeed;
+    address usdtPriceFeed;
+    mapping(address user => uint256 amount) public collateralDeposited;
     mapping(address user => Debt[]) public userDebt;
 
     ////////////// Event ////////////////
@@ -54,10 +55,15 @@ contract BLP is ReentrancyGuard {
     error BLP__InsufficentDepositToWithdraw();
     error BLP__LowCollaterizationRatio();
     error BLP__InsufficientLiquidity();
-    
-    ///////////// Constructor ////////////
     error BLP__InsufficentCollateralToWithdraw();
-    constructor(address _weth, address _usdt, address _wethPriceFeeed, address _usdtPriceFeed) {
+
+    ///////////// Constructor ////////////
+    constructor(
+        address _weth,
+        address _usdt,
+        address _wethPriceFeeed,
+        address _usdtPriceFeed
+    ) {
         wETH = IERC20(_weth);
         USDT = IERC20(_usdt);
         addressOfWeth = _weth;
@@ -73,7 +79,10 @@ contract BLP is ReentrancyGuard {
     }
 
     modifier validCollateralToken(address token) {
-        require(token == addressOfWeth, BLP__InvalidCollateralTokenAddress(token));
+        require(
+            token == addressOfWeth,
+            BLP__InvalidCollateralTokenAddress(token)
+        );
         _;
     }
 
@@ -83,18 +92,19 @@ contract BLP is ReentrancyGuard {
     }
 
     ////////////////  Public functions //////////////
-    function provideUSDTForLiquidity(address token, uint256 amount)
-        public
-        validDepositToken(token)
-        moreThanZero(amount)
-    {
+    function provideUSDTForLiquidity(
+        address token,
+        uint256 amount
+    ) public validDepositToken(token) moreThanZero(amount) {
         USDT.transferFrom(msg.sender, address(this), amount);
         userDeposits[msg.sender].push(Deposit(amount, block.timestamp));
         emit BLP_USDT_Deposited(amount);
     }
 
-    function withdrawLiquidityInUsdt(uint256 amount) public nonReentrant moreThanZero(amount) {
-        uint256 totalValue = getTotalValueOfDeposit();
+    function withdrawLiquidityInUsdt(
+        uint256 amount
+    ) public nonReentrant moreThanZero(amount) {
+        uint256 totalValue = _getTotalValueOfDeposit(msg.sender); // Check it's msg.sender
         require(amount <= totalValue, "BLP__InsufficientDepositToWithdraw");
 
         uint256 remainingAmount = amount;
@@ -104,7 +114,11 @@ contract BLP is ReentrancyGuard {
             Deposit storage deposit = deposits[i];
 
             uint256 timeElapsed = block.timestamp - deposit.timestamp;
-            uint256 interest = _calulateInterest(deposit.amount, timeElapsed);
+            uint256 interest = _calulateInterest(
+                deposit.amount,
+                timeElapsed,
+                LENDING_INTEREST_RATE
+            );
             uint256 totalDepositValue = deposit.amount + interest;
 
             if (totalDepositValue >= remainingAmount) {
@@ -125,70 +139,132 @@ contract BLP is ReentrancyGuard {
         emit BLP__USDT_Withdrawn(amount);
     }
 
-    function getTotalValueOfDeposit() public view returns (uint256 totalAmount) {
+    function depositCollateral(
+        address token,
+        uint256 amount
+    ) public moreThanZero(amount) validCollateralToken(token) {
+        wETH.transferFrom(msg.sender, address(this), amount);
+        collateralDeposited[msg.sender] += amount;
+        emit BLP__WETH_Deposited(amount);
+    }
+
+    function withdrawCollateral(
+        address token,
+        uint256 amount
+    ) public moreThanZero(amount) validCollateralToken(token) {
+        require(
+            collateralDeposited[msg.sender] > amount,
+            BLP__InsufficentCollateralToWithdraw()
+        );
+        collateralDeposited[msg.sender] -= amount;
+        _revertIfColllaterizationRatioIsLow(msg.sender);
+        wETH.transfer(msg.sender, amount);
+        emit BLP__WETH_Withdrawn(amount);
+    }
+
+    function borrowUsdt(
+        address token,
+        uint256 amount
+    ) public moreThanZero(amount) validDepositToken(token) nonReentrant {
+        userDebt[msg.sender].push(Debt(amount, block.timestamp));
+        _revertIfColllaterizationRatioIsLow(msg.sender);
+        require(
+            USDT.balanceOf(address(this)) > amount,
+            BLP__InsufficientLiquidity()
+        );
+        USDT.transfer(msg.sender, amount);
+        emit BLP__USDT_Borrowed(amount);
+    }
+
+    function getUsdValue(
+        address token,
+        uint256 amount
+    ) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            token == addressOfUsdt ? usdtPriceFeed : wethPriceFeed
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // 1 ETH = 1000 USD
+        // The returned value from Chainlink will be 1000 * 1e8
+        // Most USD pairs have 8 decimals, so we will just pretend they all do
+        return
+            ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
+    }
+
+    function getTotalValueOfDeposit()
+        public
+        view
+        returns (uint256 totalAmount)
+    {
+        _getTotalValueOfDeposit(msg.sender);
+    }
+
+    /////////////// Internal functions /////////////
+
+    function _calulateInterest(
+        uint256 amount,
+        uint256 timeElapsed,
+        uint256 INTEREST_RATE
+    ) internal pure returns (uint256 interest) {
+        interest += (amount * timeElapsed * INTEREST_RATE) / SECONDS_IN_YEAR;
+    }
+
+    function _calculateCollaterizationRatio(
+        address user
+    ) internal view returns (uint256 ratio) {
+        ratio =
+            (getUsdValue(addressOfUsdt, collateralDeposited[user]) * 1e18) /
+            getUsdValue(addressOfUsdt, _calculateTotalDebt(user));
+        //should be greater than 1.5e18
+    }
+
+    function _calculateTotalDebt(
+        address user
+    ) internal view returns (uint256 totalAmount) {
+        uint256 length = getDebts(msg.sender).length;
+        for (uint256 i = 0; i < length; i++) {
+            uint256 amount = getDebts(msg.sender)[i].amount;
+            uint256 timestamp = getDebts(msg.sender)[i].timestamp;
+            uint256 timeElapsed = block.timestamp - timestamp;
+            uint256 interest = _calulateInterest(
+                amount,
+                timeElapsed,
+                BORROWING_INTEREST_RATE
+            );
+            totalAmount += amount + interest;
+        }
+    }
+
+    function _revertIfColllaterizationRatioIsLow(address user) internal view {
+        uint256 ratio = _calculateCollaterizationRatio(user);
+        // 1.5e18 = 15e17
+        require(ratio > 15e17, BLP__LowCollaterizationRatio());
+    }
+
+    function _getTotalValueOfDeposit(
+        address user
+    ) internal view returns (uint256 totalAmount) {
         uint256 length = getDeposits(msg.sender).length;
         for (uint256 i = 0; i < length; i++) {
             uint256 amount = getDeposits(msg.sender)[i].amount;
             uint256 timestamp = getDeposits(msg.sender)[i].timestamp;
             uint256 timeElapsed = block.timestamp - timestamp;
-            uint256 interest = _calulateInterest(amount, timeElapsed);
+            uint256 interest = _calulateInterest(
+                amount,
+                timeElapsed,
+                LENDING_INTEREST_RATE
+            );
             totalAmount += amount + interest;
         }
     }
-
-    function depositCollateral (address token, uint256 amount) public moreThanZero(amount) validCollateralToken(token){
-        wETH.transferFrom(msg.sender,address(this),amount);
-        collateralDeposited[msg.sender] += amount;
-        emit BLP__WETH_Deposited(amount);
-    }
-
-    function withdrawCollateral (address token, uint256 amount) public moreThanZero(amount) validCollateralToken(token){
-        require(collateralDeposited[msg.sender]>amount , BLP__InsufficentCollateralToWithdraw());
-        collateralDeposited[msg.sender] -= amount;
-        _revertIfColllaterizationRatioIsLow(msg.sender);
-        wETH.transfer(msg.sender,amount);
-        emit BLP__WETH_Withdrawn(amount);
-    }
-
-    function borrowUsdt (address token, uint256 amount) public moreThanZero(amount) validDepositToken(token) nonReentrant{
-        userDebt[msg.sender].push(Deposit(amount,block.timestamp));
-        _revertIfColllaterizationRatioIsLow(msg.sender);
-        require(USDT.balanceOf(address(this))> amount, BLP__InsufficientLiquidity());
-        USDT.transfer(msg.sender,amount);
-        emit BLP__USDT_Borrowed(amount);
-    }
-
-    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(token == addressOfUsdt ? usdtPriceFeed : wethPriceFeed);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        // 1 ETH = 1000 USD
-        // The returned value from Chainlink will be 1000 * 1e8
-        // Most USD pairs have 8 decimals, so we will just pretend they all do
-        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
-    }
-    /////////////// Internal functions /////////////
-
-    function _calulateInterest(uint256 amount, uint256 timeElapsed) internal pure returns (uint256 interest) {
-        interest += (amount * timeElapsed * INTEREST_RATE) / SECONDS_IN_YEAR;
-    }
-
-    //Need to take care of interest on debt now
-    function _calculateCollaterizationRatio(address user) internal view returns(uint256 ratio){
-        ratio = (getUsdValue(addressOfUsdt,collateralDeposited[user])*1e18)/getUsdValue(addressOfUsdt, UsdtBorrowed[user]);
-        //should be greater than 1.5e18
-    }
-
-    function _revertIfColllaterizationRatioIsLow(address user) internal view{
-        uint256 ratio = _calculateCollaterizationRatio(user);
-        // 1.5e18 = 15e17
-        require(ratio > 15e17 , BLP__LowCollaterizationRatio());
-    }
-
-
 
     ////////////// Getter Functions ///////////////
 
     function getDeposits(address user) public view returns (Deposit[] memory) {
         return userDeposits[user];
+    }
+
+    function getDebts(address user) public view returns (Debt[] memory) {
+        return userDebt[user];
     }
 }
